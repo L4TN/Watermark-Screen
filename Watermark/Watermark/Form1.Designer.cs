e52ef88ddb15b4ab4e49717b706c144515bbc3f6
Matheus Sousa Dias <75400361+L4TN@users.noreply.github.com>
2022-03-03 11:07:04 -0300

    Enviando Exemplos
    Exemplos dos StackOverflow de Timer e de Screen Watermark para serem implementados

---
+++ Watermark/Watermark/Form1.Designer.cs
@@ -0,0 +1,53 @@
+﻿namespace Watermark
+{
+    partial class Watermark
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Watermark
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 30F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.ClientSize = new System.Drawing.Size(1721, 979);
+            this.Font = new System.Drawing.Font("Microsoft YaHei UI", 17.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.ForeColor = System.Drawing.Color.Chocolate;
+            this.Margin = new System.Windows.Forms.Padding(7);
+            this.Name = "Watermark";
+            this.Opacity = 0.99D;
+            this.Text = "Watermark";
+            this.TransparencyKey = System.Drawing.Color.Gray;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+        #endregion
+    }
+}
---
+++ Watermark/Watermark/Form1.cs
@@ -0,0 +1,76 @@
+using System.Timers;
+
+namespace Watermark
+{
+    public partial class Watermark : Form
+    {
+        public string User = "HS PREVENT \r\n" + "Direitos reservados \r\n" + Environment.UserName + "\r\n" + Environment.MachineName + "\r\n";
+        private const int WS_EX_TRANSPARENT = 0x20;
+
+        public Watermark()
+        {
+            InitializeComponent();
+            this.Opacity = .30;
+            this.TopMost = true;
+            this.WindowState = FormWindowState.Maximized;
+            this.Paint += Watermark_Paint;
+            //this.FormBorderStyle = FormBorderStyle.SizableToolWindow;
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.ShowInTaskbar = false;
+            this.TopMost = true;
+
+        }
+        // this makes the form ignore all clicks, so it is "passthrough"
+        protected override System.Windows.Forms.CreateParams CreateParams
+        {
+            get
+            {
+                CreateParams cp = base.CreateParams;
+                cp.ExStyle = cp.ExStyle | WS_EX_TRANSPARENT; //Define a Transparencia de Fundo
+                cp.ExStyle |= 0x80; //Hide Borderless Form from Alt+Tab
+                return cp;
+            }
+        }
+
+        public string hora = DateTime.Now.ToLongTimeString();
+        public string data = DateTime.Now.ToString("dd/MM/yyyy");
+        
+        private void Watermark_Paint(object sender, PaintEventArgs e)
+        {
+           
+
+            if (User != "")
+                {
+                    // play with this drawing code to change your "watermark"  
+                    SizeF szF1 = e.Graphics.MeasureString(User + DateTime.Now.ToLongTimeString() + DateTime.Now.ToString("dd/MM/yyyy"), this.Font);
+                    int max = Math.Max(this.Width, this.Height);
+                
+                    //Tira o serrilhado da fonte
+                    e.Graphics.TextRenderingHint = System.Drawing.Text.TextRenderingHint.AntiAliasGridFit;
+                    e.Graphics.CompositingQuality = System.Drawing.Drawing2D.CompositingQuality.HighQuality;
+                    e.Graphics.TextRenderingHint =System.Drawing.Text.TextRenderingHint.SingleBitPerPixel;
+                   
+
+                        for (int y = 0; y <= max; y = y + (2 * (int)szF1.Height))
+                        {
+                            for (int x = 0; x <= max; x = x + (3 * (int)szF1.Height))
+                            {
+                                //e.Graphics.DrawString(dados, this.Font, Brushes.Black, 0, y);
+                                var test = new SolidBrush(Color.FromArgb(255, Color.LightGray));
+                                e.Graphics.DrawString(User + DateTime.Now.ToLongTimeString()  , this.Font, test, x, y);
+                                
+                            }
+                        }
+                    
+                    
+                }
+            
+            
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        { 
+
+        }
+
+    }
+}
